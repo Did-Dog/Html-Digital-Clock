94c923b33d23b16ac1a6bc8aae8eae85f0bbc721
DidDog <94751052+Did-Dog@users.noreply.github.com>
2021-12-31 15:42:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ var today = new Date();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 	document.getElementById('currentTime').innerHTML = time;
 }
+function showDate() {
+var today = new Date();
+var time = today.getDate() + "-" + today.getMonth()+1) + "-" + today.getFullYear();
+	document.getElementById('currentDate').innerHTML = time;
+}
 showTime();
+showDate();
 setInterval(function () {
 	showTime();
+        showDate();
 }, 1000);
